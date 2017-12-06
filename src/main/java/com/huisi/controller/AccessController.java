dd537bf77e8afd2e323ce8a904223e2f9068c49c
xuhan <xuhan@droi.com>
2017-12-06 18:34:23 +0800

    项目结构更新20171206

---
+++ src/main/java/com/huisi/controller/AccessController.java
@@ -0,0 +1,16 @@
+package com.huisi.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xuhan on 2017/12/5.
+ */
+@RestController
+public class AccessController {
+
+    @PostMapping("/login")
+    public void doLogin() {
+
+    }
+}
---
+++ src/main/java/com/huisi/model/UserBo.java
@@ -0,0 +1,25 @@
+package com.huisi.model;
+
+/**
+ * Created by xuhan on 2017/12/5.
+ */
+public class UserBo {
+    private String userName;
+    private String passWord;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
