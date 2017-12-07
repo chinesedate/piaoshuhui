587d1b7949054975adcc81dd961b11955ce15d34
xuhan <xuhan@droi.com>
2017-12-07 17:36:44 +0800

    添加DispatcherServlet的启动配置

--- src/main/java/com/huisi/controller/AccessController.java
+++ src/main/java/com/huisi/controller/AccessController.java
@@ -1,6 +1,6 @@
 package com.huisi.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccessController {
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public void doLogin() {
-
+        int a = 0;
     }
 }
