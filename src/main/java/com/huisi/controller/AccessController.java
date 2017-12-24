eadf969fa3826bc457321fad24f6847b50c18fa0
xuhan <3199088399@qq.com>
2017-12-24 12:01:32 +0800

    jsp+mysql配置成功

--- src/main/java/com/huisi/controller/AccessController.java
+++ src/main/java/com/huisi/controller/AccessController.java
@@ -3,23 +3,35 @@ package com.huisi.controller;
 import com.huisi.model.UserBo;
 import com.huisi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by xuhan on 2017/12/5.
  */
-@RestController
+@Controller
 public class AccessController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/login")
-    public void doLogin() {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String doLogin(
+            @RequestParam("userName") String userName,
+            @RequestParam("password") String password
+    ) {
         UserBo userBo = new UserBo();
         userBo.setUserName("testName");
         userBo.setPassword("123qwe");
         userService.insertBo_USER(userBo);
+        return "index";
     }
 }
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<html>
+<head>
+</head>
+<body>
+<form name="login" action="/login" method="post">
+    用户名：<input type="text" name="userName"/> </br>
+    密码：<input type="password" name="password"/> </br>
+    <input type="submit" value="提交">
+
+</form>
+</body>
+</html>
